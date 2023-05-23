c9fd5c1f9c4db000cda4a77c8b67d067dec6b153
R3RO <paulocesarbritocamilo@gmail.com>
2023-05-23 10:28:39 -0300

    Versão beta 2.0

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const menu1= window.document.getElementById("menu")
+const menu1 = window.document.getElementById("menu")
 const menu2 = window.document.getElementById("menu2")
 const nav  = window.document.getElementById("nav")
 
+/*  Sessão personagens breaking bad */
+
+const sessao1 = window.document.getElementById("sessao-personagens-1")
+const personagens1 = window.document.getElementById ("personagens-1")
+const personagens2 = window.document.getElementById ("personagens-2")
+const personagens3 = window.document.getElementById ("personagens-3")
+const personagens4 = window.document.getElementById ("personagens-4")
+const personagens5 = window.document.getElementById ("personagens-5")
+
+
+ /* Menu do site */
 
 menu1.addEventListener ('click' , function menu(){
     nav.style.display = "inline-block"
@@ -17,3 +28,28 @@ menu2.addEventListener ('click' , function menu(){
 
 
 })
+
+
+   personagens1.addEventListener("click", function SessaoPersonagens(sessao01){
+
+   sessao1.style.display = "block"
+
+})
+
+personagens2.addEventListener("click", function SessaoPersonagens(sessao02){
+    sessao1.style.display = "none"
+})
+
+personagens3.addEventListener("click", function SessaoPersonagens(sessao3){
+    
+})
+
+personagens4.addEventListener("click", function SessaoPersonagens(sessao4){
+    
+})
+
+personagens5.addEventListener("click", function SessaoPersonagens(sessao1){
+    
+})
+
+
